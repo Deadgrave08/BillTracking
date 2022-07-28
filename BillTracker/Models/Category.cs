ed188627a3cf69d3ba0949da9b1917607eaba61f
Mohd Arif <mohd.arif3@globallogic.com>
2022-07-28 10:54:58 +0530

    First commit

---
+++ BillTracker/Models/Category.cs
@@ -0,0 +1,47 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace BillTracker.Models
+{
+    public class Category
+    {
+        [Key]
+        public int categoryId { get; set; }
+        [Column(TypeName = "nvarchar(50)")]
+        public string Title { get; set; }
+        [Column(TypeName = "nvarchar(5)")]
+        public string Icon { get; set; } = "";
+        [Column(TypeName = "nvarchar(10)")]
+        public string Type { get; set; } = "Expense";
+        [NotMapped]
+        public string? TitleWithIcon
+        {
+            get
+            {
+                return this.Icon + " " + this.Title;
+            }
+        }
+    }
+    public class Transaction
+    {
+        [Key]
+        public int TransactionId { get; set; }
+        public int CategoryId { get; set; }
+        public Category category { get; set; }
+        public int Amount { get; set; }
+        [Column(TypeName = "nvarchar(75)")]
+        public string? Note { get; set; }
+        public DateTime Date { get; set; } = DateTime.Now;
+    }
+
+    public class BillContext : DbContext
+    {
+        public BillContext(DbContextOptions<BillContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Transaction> Transactions { get; set; }
+    }
+}
